30a7734aca353dabebcf7e5f403b013db5fd84fc
Naitort <naitora00@mail.ru>
2024-02-23 16:32:56 +0300

    Проверка на конлфикты

--- sudoku.js
+++ sudoku.js
@@ -1,26 +1,25 @@
 const fs = require('fs')
 
 function read(n) {
+    let arrNum = []
+    const arr = fs
+        .readFileSync('./puzzles.txt', 'utf-8')
+        .trim()
+        .split('\n')
+        [n].split('')
 
+    for (let i = 0; i < arr.length; i += 9) {
+        arrNum.push(arr.slice(i, i + 9))
+    }
+    return arrNum
 }
-=======
-  let arrNum =[]
-const arr =fs.readFileSync('./puzzles.txt', 'utf-8').trim().split('\n')[n].split('')
+//console.log(read(3))
 
-for (let i = 0; i < arr.length; i+=9) {
-  console.log(arr[i])
-arrNum.push(arr.slice(i, i+9))
-}
-return arrNum
-}
-console.log(read(3))
-
-  /**
-   * cсчитываем строчку через фску
-   * сформировать массив 9х9
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
-
+/**
+ * cсчитываем строчку через фску
+ * сформировать массив 9х9
+ * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
+ */
 
 function solve(read) {
     //составляем массив строк по горизонтали
@@ -39,21 +38,19 @@ function solve(read) {
 
             let newArrInside = []
             for (let j = 0; j < newRead.length; j++) {
-                console.log(read[j][i])
-
                 newArrInside.push(read[j][i])
             }
             newArr.push(newArrInside)
         }
-        console.log(newArr)
+
         return newArr
     }
 
-    readToStringVert(read, 77)
+    //   readToStringVert(read, 77)
 
     //находим каких цифр нет в строке
     const numbers = function doNumbers() {
-        const numbers = ['a', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        const numbers = ['f', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         const str = arr.join('').replaceAll('-', '')
         const newArr = []
         numbers.forEach((el) => !str.includes(el) && newArr.push(el))
@@ -75,18 +72,13 @@ function isSolved() {
      * Принимает игровое поле в том формате, в котором его вернули из функции solve.
      * Возвращает булевое значение — решено это игровое поле или нет.
      */
+    return 'Is totally solved'
 }
 
-function prettyBoard() {
+function prettyBoard(n) {
     /**
      * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-     * Выводит в консоль/терминал судоку.
-     * Подумай, как симпатичнее его вывести.
-     */
-
-    for (item of solve()) {
-        const sudoku = item.map((el) => emojis[el]).join('')
-        board2.push(emoj)
-    }
-    return sudoku.join('\n')
+     * Выводит в консоль/терминал судоку.*/
+    return n.join('\n')
 }
+//console.log(prettyBoard(read(3)))
